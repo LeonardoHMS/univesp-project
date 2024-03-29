7073ebac5fbf6658068461ed02ddd2fbded81e53
Leonardo Mantovani <leorique2010@hotmail.com>
2024-03-29 10:10:17 -0300

    add files

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Schedule, TypeOfCut
+
+
+class TypeOfCutAdmin(admin.ModelAdmin):
+    list_display = ("type", "value")
+    search_fields = ("type",)
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ("day", "hour", "type_of_cut", "user")
+    search_fields = ("day", "type_of_cut")
+
+
+admin.site.register(TypeOfCut, TypeOfCutAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TypeOfCut(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=255)
+    value = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.type
+
+
+class Schedule(models.Model):
+    id = models.AutoField(primary_key=True)
+    day = models.DateField()
+    hour = models.TimeField()
+    type_of_cut = models.ForeignKey(
+        TypeOfCut,
+        on_delete=models.PROTECT,
+    )  # Noqa
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"Agendado {self.day}"
