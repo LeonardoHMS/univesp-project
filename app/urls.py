852ebd662761cee7af1978fe13632bef0ad40706
Leonardo Mantovani <leorique2010@hotmail.com>
2024-04-04 21:44:12 -0300

    commit

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from schedule.views import home_view
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", home_view, name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
